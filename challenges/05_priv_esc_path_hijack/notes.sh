d95623f14b16755464b5fc898427fb340e2f6054
cobaltsixd <mickeyfbarry@gmail.com>
2025-09-29 17:00:11 -0400

    privesc: updated message

--- challenges/05_priv_esc_path_hijack/notes.sh
+++ challenges/05_priv_esc_path_hijack/notes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # intentionally calls 'sh' without full path to show PATH hijack vector
-sh -c 'echo "[notes] backing up..." && tar -cf /tmp/notes.tar /etc/hosts >/dev/null 2>&1 && echo done'
+sh -c 'echo "[notes] backing up..." && tar -cf /tmp/notes.tar /etc/hosts >/dev/null 2>&1 && echo I SED i am done'
